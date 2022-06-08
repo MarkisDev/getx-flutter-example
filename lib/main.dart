5a2350c848686aba32cdc5f622e853af2c2a69c0
Rijuth Menon <rijuthm@gmail.com>
2022-06-08 23:23:35 +0530

    added main.dart with GetMaterialApp and GetPages

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:getx_authentication_google/bindings/authentication_binding.dart';
+import 'package:getx_authentication_google/views/home_view.dart';
+import 'package:getx_authentication_google/views/login_view.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const LoginPage());
+}
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      initialRoute: '/',
+      getPages: [
+        GetPage(
+            name: '/',
+            page: () => LoginView(),
+            binding: AuthenticationBinding()),
+        GetPage(
+            name: '/home',
+            page: () => HomeView(),
+            binding: AuthenticationBinding())
+      ],
+    );
+  }
+}
