562e5408cbd632924f061df8749a2e4751e6f618
Rijuth Menon <rijuthm@gmail.com>
2022-06-09 20:47:05 +0530

    added home binding

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:getx_authentication_google/bindings/authentication_binding.dart';
+import 'package:getx_authentication_google/bindings/home_binding.dart';
 import 'package:getx_authentication_google/views/home_view.dart';
 import 'package:getx_authentication_google/views/login_view.dart';
 
@@ -23,10 +24,7 @@ class LoginPage extends StatelessWidget {
             name: '/',
             page: () => LoginView(),
             binding: AuthenticationBinding()),
-        GetPage(
-            name: '/home',
-            page: () => HomeView(),
-            binding: AuthenticationBinding())
+        GetPage(name: '/home', page: () => HomeView(), binding: HomeBinding())
       ],
     );
   }
