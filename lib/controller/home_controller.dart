4ee0a273b1489fdccbf7acc1e79a4fb7eab90c7e
Rijuth Menon <rijuthm@gmail.com>
2022-06-09 20:47:33 +0530

    added home controller

---
+++ lib/controller/home_controller.dart
@@ -0,0 +1,18 @@
+import 'package:get/get.dart';
+import 'package:getx_authentication_google/models/user_model.dart';
+
+class HomeController extends GetxController {
+  final UserModel userModel = Get.arguments;
+  @override
+  void onInit() {
+    super.onInit();
+  }
+
+  @override
+  void onReady() {
+    super.onReady();
+  }
+
+  @override
+  void onClose() {}
+}
